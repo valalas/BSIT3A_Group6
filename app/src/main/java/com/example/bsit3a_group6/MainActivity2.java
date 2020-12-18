3519ced909e6e50b2eb5c7e9a104e627d63f43f5
valalas <jericacevalmadrid@gmail.com>
2020-12-19 00:02:53 +0800

    Update: 3 screen Project

--- app/src/main/java/com/example/bsit3a_group6/MainActivity2.java
+++ app/src/main/java/com/example/bsit3a_group6/MainActivity2.java
@@ -2,13 +2,30 @@ package com.example.bsit3a_group6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+    }
+
+    private void openActivity3() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
     }
 }
